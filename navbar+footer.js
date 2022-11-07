1b065ee05af41e8a5e3ceb742dfb8291286c77d8
CoderZIE <102377644+CoderZIE@users.noreply.github.com>
2022-11-07 19:31:45 +0530

    Add files via upload

--- navbar+footer.js
+++ navbar+footer.js
@@ -1,5 +1,6 @@
+document.querySelector(".zwrap").style.display = "none"
 function preloaderFunction() {
 
-    document.querySelector(".Rpreload").style.display = "none"
-  
-  }
+  document.querySelector(".Rpreload").style.display = "none"
+  document.querySelector(".zwrap").style.display = "initial"
+}
